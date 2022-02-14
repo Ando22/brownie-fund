19ac21f0748070751cb32f3efaf8e1fb5b6ec03b
Erando Putra <mufasa@Erandos-MacBook-Pro.local>
2022-02-14 17:01:47 +0700

    init brownie test

---
+++ scripts/deploy.py
@@ -0,0 +1,26 @@
+from brownie import FundMe, network, config, MockV3Aggregator
+from scripts.helpful import (deploy_mocks, get_account, LOCAL_BLOCKCHAIN_ENVIRONTMENT)
+
+
+def deploy_fund_me():
+    account = get_account()
+    # if using presistent network like rinkeby
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONTMENT:
+        price_feed_address = config["networks"][network.show_active()][
+            "eth_usd_price_feed"
+        ]
+    else:
+        deploy_mocks()
+        price_feed_address = MockV3Aggregator[-1].address
+        print("Mocks Deployed")
+
+    fund_me = FundMe.deploy(
+        price_feed_address,
+        {"from": account}, 
+        publish_source=config["networks"][network.show_active()].get("verify")
+    )
+    print(f"contract Deployed to {fund_me.address}")
+    return fund_me
+
+def main():
+    deploy_fund_me()
